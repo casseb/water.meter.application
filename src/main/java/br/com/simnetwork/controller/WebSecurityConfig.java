c0da7aa0fc10b7fa4b4720832399370b0ac5640c
casseb <felipe.casseb@gmail.com>
2017-11-27 21:30:30 -0200

    Corrigindo zicas

---
+++ src/main/java/br/com/simnetwork/controller/WebSecurityConfig.java
@@ -0,0 +1,51 @@
+package br.com.simnetwork.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import br.com.simnetwork.model.entity.Usuario;
+import br.com.simnetwork.model.service.UsuarioService;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	private UsuarioService usuarioService;
+
+	@Override
+	protected void configure(HttpSecurity httpSecurity) throws Exception {
+		httpSecurity.csrf().disable().authorizeRequests()
+			.antMatchers(HttpMethod.POST, "/login").permitAll()
+			.anyRequest().authenticated()
+			.and()
+			
+			// filtra requisições de login
+			.addFilterBefore(new JWTLoginFilter("/login", authenticationManager()),
+	                UsernamePasswordAuthenticationFilter.class)
+			
+			// filtra outras requisições para verificar a presença do JWT no header
+			.addFilterBefore(new JWTAuthenticationFilter(),
+	                UsernamePasswordAuthenticationFilter.class);
+	}
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		
+		for(Usuario usuario : usuarioService.getAllUsers()) {
+			auth.inMemoryAuthentication()
+			.withUser(usuario.getLogin())
+			.password(usuario.getSenha())
+			.roles("ADMIN");
+		}
+		
+	}
+}
